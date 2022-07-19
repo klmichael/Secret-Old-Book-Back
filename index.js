230fb972b62d2b843b826914563b532614937d09
klmichael <87207684+klmichael@users.noreply.github.com>
2022-07-19 16:54:09 -0500

    Added PostSQL queries

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./queries');
 const app = express();
 const port = 3000;
 
@@ -9,6 +10,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (request, response) => {
   response.json({ info: 'How old is your Nancy Drew book?'});
 });
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
